9f69e5d4d8d587d2898d4feaa0253075ff7fd9aa
Asher <asheressani@gmail.com>
2024-10-30 23:50:27 +0500

    Web Build!

--- lib/presentation/flutter_network_repository.dart
+++ lib/presentation/flutter_network_repository.dart
@@ -24,7 +24,6 @@ class FlutterNetworkRespository extends StatelessWidget {
             virtualKeyboard: false,
           ),
         ],
-        devices: devices,
         builder: (context) {
           return ScreenUtilInit(
               useInheritedMediaQuery: true,
@@ -40,20 +39,3 @@ class FlutterNetworkRespository extends StatelessWidget {
         });
   }
 }
-
-final devices = [
-  Devices.android.samsungGalaxyA50,
-  Devices.android.samsungGalaxyNote20,
-  Devices.android.samsungGalaxyS20,
-  Devices.android.samsungGalaxyNote20Ultra,
-  Devices.android.onePlus8Pro,
-  Devices.android.sonyXperia1II,
-  Devices.ios.iPhoneSE,
-  Devices.ios.iPhone12,
-  Devices.ios.iPhone12Mini,
-  Devices.ios.iPhone12ProMax,
-  Devices.ios.iPhone13,
-  Devices.ios.iPhone13ProMax,
-  Devices.ios.iPhone13Mini,
-  Devices.ios.iPhoneSE,
-];
